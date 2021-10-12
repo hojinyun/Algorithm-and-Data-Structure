ebbe5953b3bde9363fe0941a8da5ea955ffd1028
Hojin Yun <hojinyun.dev@gmail.com>
2021-10-12 18:40:09 +0900

    solved problem #2751

---
+++ Baek Joon Problems/#2751 수 정렬하기 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int main(void){
+	int n;
+	cin >> n;
+	
+	int numbers[n];
+	
+	for(int i = 0; i < n; i++){
+		cin >> numbers[i];
+	}
+	sort(numbers, numbers+n);
+	for(int i = 0; i < n; i++){
+		cout << numbers[i] << '\n'; // '\n' 안쓰면 실행 안됨 
+	}
+}
