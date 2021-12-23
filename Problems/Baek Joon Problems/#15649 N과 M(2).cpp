e0636a1c76cc2544558dcf722b77afff5a5308b6
Hojin Yun <hojinyun.dev@gmail.com>
2021-12-23 17:43:34 +0900

    solved N과 M(2) problem

---
+++ Problems/Baek Joon Problems/#15649 N과 M(2).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, c[9], a[9];
+
+void brute_force(int index){
+	if(index == m){
+		for(int i = 0; i < m; i++){
+			cout << a[i] << " ";
+		} 
+		cout << "\n";
+		return;
+	}
+	for(int i = 1; i <= n; i++){
+		if(c[i] || i < a[index-1]) continue;
+		a[index] = i;
+		c[i] = 1;
+		brute_force(index+1);
+		c[i] = 0;
+		a[index] = 0;
+	}
+}
+
+int main(void){
+	cin >> n >> m;
+	brute_force(0);
+	
+}
