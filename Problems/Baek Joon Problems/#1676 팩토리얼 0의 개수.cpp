9247cc2e72d5cefe6ed1bbe49459ff7f1c32a55f
Hojin Yun <hojinyun.dev@gmail.com>
2022-01-29 16:11:24 +0800

    solved 팩토리얼 0의 개수 problem

--- Problems/Baek Joon Problems/#1676 팩토리얼 0의 개수.cpp
+++ Problems/Baek Joon Problems/#1676 팩토리얼 0의 개수.cpp
@@ -6,13 +6,13 @@ int main(){
 	int n, answer = 0;
 	//입력
 	cin >> n;
-	//n에서 하나씩 빼면서 5를 약수로 몇개 갖고 있는지 확인 
+	//n에서 하나씩 빼면서 5를 약수로 몇개 갖고 있는지 확인
 	while(n){
 		if(!(n%125)) answer+=3; //5^3이므로 +3 
 		else if(!(n%25)) answer+=2; //5^2이므로 +2
 		else if(!(n%5)) answer+=1; //5^1이므로 +1
 		n--;
 	}
-	//출력 
+	//출력
 	cout<<answer;
 }
