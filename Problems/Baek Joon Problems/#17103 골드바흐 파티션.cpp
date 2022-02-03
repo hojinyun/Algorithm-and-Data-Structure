22e41321a03499ef71eb1db64e64d2132c9194f8
Hojin Yun <hojinyun.dev@gmail.com>
2022-02-03 23:08:08 +0800

    added comments to the 골드바흐 파티션 solution

--- Problems/Baek Joon Problems/#17103 골드바흐 파티션.cpp
+++ Problems/Baek Joon Problems/#17103 골드바흐 파티션.cpp
@@ -8,23 +8,24 @@ bool isPrime[MAX] = {false, false};
 
 int main(){
 	int n, number, counter = 0;
+	//입력 
 	cin >> n;
 	//에라토스테네스의 체
-	//값 초기화 
+	//배열값 모두 true로 초기화 
 	for(int i = 2; i < MAX; i++) isPrime[i] = true;
+	//배수인 경우에 false로 변환 
 	for(int i = 2; i < MAX; i++){
 		if(isPrime[i] == false) continue;
 		for(int j = i+i; j < MAX; j+=i) isPrime[j] = false;
 	}
-	
+	//합하여 입력한 수가 나오는 소수가 있을 때 마다 카운터를 올린다 
 	for(int i = 0; i < n; i++){
 		cin >> number;
 		for(int j = 2; j <= number/2;j++){
 			if(isPrime[j] && isPrime[number-j]) counter++;
 		}
+		//정답 출력 
 		cout << counter << '\n';
 		counter = 0; //counter 값 초기화 
 	}
-	
-	
 }
