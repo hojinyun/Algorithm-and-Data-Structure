e958f848f4909efea05904b84a40029c01a53f81
Hojin Yun <hojinyun.dev@gmail.com>
2022-02-28 23:14:08 +0800

    added missing comments to 가장 긴 증가하는 부분 수열 problem

--- Problems/Baek Joon Problems/#11053 가장 긴 증가하는 부분 수열.cpp
+++ Problems/Baek Joon Problems/#11053 가장 긴 증가하는 부분 수열.cpp
@@ -4,16 +4,18 @@ using namespace std;
 
 int main(){
 	int n, ans = 0, arr[1001], d[1001]={};
+	//입력
 	cin >> n;
 	for(int i = 0; i<n; i++){
-		int max_num = 0;
+		int max_num = 0; //이전 최대 값과 중첩 안되게 최대 값 0으로 초기화
 		cin >> arr[i];
 		for(int j = 0; j<i; j++){
-			if(arr[i] > arr[j]) max_num = max(max_num, d[j]);
+			//현재 배열보다 작은 값인 경우 그 중 d[]에 저장 된 최대 길이의 값을 기록
+			if(arr[j] > arr[i]) max_num = max(max_num, d[j]);
 		}
-		d[i] = max_num+1;
-		ans = max(ans, d[i]);
+		d[i] = max_num+1; //자기 자신을 포함하므로 최대 길이 +1
+		ans = max(ans, d[i]); //전체 입력값 중에 최대 길이를 기록
 	}
-	
+	//출력
 	cout << ans;
 }
